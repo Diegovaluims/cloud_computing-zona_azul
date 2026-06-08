84b94700b1fd2c26e5ba9e5fd2eca56ccc9e5ace
Diegovaluims <diegoluisvalim@icloud.com>
2026-06-08 12:55:52 -0300

    fix: changed aws instace to a valid (free tier) one

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "zona_azul_sg" {
 # 3. Criação da Instância EC2
 resource "aws_instance" "backend_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro" # Elegível para o plano gratuito
+  instance_type = "t3.micro" # Elegível para o plano gratuito
 
   # Atrela o grupo de segurança criado acima à máquina
   vpc_security_group_ids = [aws_security_group.zona_azul_sg.id]
